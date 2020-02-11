762a7b4e6e2ad0327b99e6df74f5ba95e8a9a66e
wesnjazz <dongwonpark@umass.edu>
2020-02-11 16:04:55 -0500

    Saving last work

--- 2D-Simulator/main.cpp
+++ 2D-Simulator/main.cpp
@@ -1,6 +1,17 @@
 #include "Map.h"
+#include <iostream>
 
 int main(int argc, char **argv)
 {
+    Map map;
+
+    map.occupy(Coordinate(0,0));
+    map.occupy(Coordinate(1,1));
+    map.occupy(Coordinate(2,2));
+    
+    map.print_points();
+    std::cout << map.is_occupied(Coordinate(2,2)) << "\n";
+    std::cout << map.is_occupied(Coordinate(2,1)) << "\n";
+
     return 0;
 }
