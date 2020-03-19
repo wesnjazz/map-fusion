36063557f5faf760554bd25870e5de9102f5f407
wesnjazz <dongwonpark@umass.edu>
2020-03-19 16:10:47 -0400

    [Update] - Robot frame grid

--- catkin_ws_Indep.Study/src/mapping_simulator/src/mapping_simulator_node.cpp
+++ catkin_ws_Indep.Study/src/mapping_simulator/src/mapping_simulator_node.cpp
@@ -209,6 +209,8 @@ int main(int argc, char **argv)
                 /** Move the robot **/
                 robot_ideal.move_to(new_position);
 
+
+                /** Calculate arrival_R: arrival_W in robot frame **/
                 Mat3f new_HT_inverse = new_HT_inverse.setIdentity();
                 Mat2f new_HT_ROT = new_HT.block<2, 2>(0, 0);
                 Vec2f new_HT_TRANS = new_HT.block<2, 1>(0, 2);
@@ -216,20 +218,25 @@ int main(int argc, char **argv)
                 Vec2f TRANS_in_inverse = new_HT_ROT_neg.transpose() * new_HT_TRANS;
                 new_HT_inverse.block<2, 2>(0, 0) = new_HT_ROT.transpose();
                 new_HT_inverse.block<2, 1>(0, 2) = TRANS_in_inverse;
-                Vec2f arrival_W2f = arrival_W.block<2, 1>(0, 0);
-                Vec3f arrival_R = new_HT_inverse * arrival_W2f.homogeneous();
-                // float angle_difference = 
-                lidar_msg.points_x.push_back(arrival_R.x());
-                lidar_msg.points_y.push_back(arrival_R.y());
-                lidar_msg.points_col.push_back(0xFF3388AA);
+                Vec2f arrival_W_only_xy = arrival_W.block<2, 1>(0, 0);
+                Vec3f arrival_R = new_HT_inverse * arrival_W_only_xy.homogeneous();
 
+                /** Get angle of goal point in robot frame **/
+                float angle_goal_robot_frame_degree = cut_redundant_epsilon( radian_to_degree( atan2(arrival_R.y(), arrival_R.x()) ));
+
+                lidar_msg.circles_x.push_back(arrival_W.x());
+                lidar_msg.circles_y.push_back(arrival_W.y());
+                lidar_msg.circles_col.push_back(0xFFFF00FF);
+
+
+                
                 cout 
                     << "Departure: (" << departure_W.x() << ", " << departure_W.y() << ")" << endl
                     << "Arrival_W: (" << arrival_W.x() << ", " << arrival_W.y() << ")" << endl
                     << "Arrival_R: (" << arrival_R.x() << ", " << arrival_R.y() << ")" << endl
                     << "  Current: (" << robot_ideal.position_in_Wframe.x() << ", " << robot_ideal.position_in_Wframe.y() << ")" << endl
-                    << " Robot's heading: " << robot_ideal.heading_degree_in_Wframe << endl
-                    << "Angle difference: " << robot_ideal.heading_degree_in_Wframe << endl
+                    << " Robot's heading in W: " << robot_ideal.heading_degree_in_Wframe << endl
+                    << "   Angle to goal in R: " << angle_goal_robot_frame_degree << endl
                     ;
 
 
@@ -239,8 +246,8 @@ int main(int argc, char **argv)
                 lidar_msg.points_col.push_back(0xFFFF0000);
 
 
-                lidar_msg.lines_p1x.push_back(arrival_W2f.x());
-                lidar_msg.lines_p1y.push_back(arrival_W2f.y());
+                lidar_msg.lines_p1x.push_back(arrival_W_only_xy.x());
+                lidar_msg.lines_p1y.push_back(arrival_W_only_xy.y());
                 lidar_msg.lines_p2x.push_back(robot_ideal.position_in_Wframe.x());
                 lidar_msg.lines_p2y.push_back(robot_ideal.position_in_Wframe.y());
                 lidar_msg.lines_col.push_back(0x229999FF);
@@ -268,164 +275,8 @@ int main(int argc, char **argv)
             /** Update the next departure waypoint to the current position **/
             departure_W = arrival_W;
 
-
-
-
-
-            // deque<Eigen::Matrix3f> homos;
-            /** Calculate curve points(trajectories) from current waypoint to the next waypoint **/
-            // Noise trajectories_noise = Noise(0.0, 0.01);        // For simulating noises on Odometry
-            /** Separate curve points.  Ideal: Where robot think it is.  Actual: Actual position of the robot **/
-            // deque<Vec2f> trajectories_ideal = interpolate_curve_points(homos, delta_t, robot_ideal, departure_W, arrive);
-            // deque<Vec2f> trajectories_actual = interpolate_curve_points(homos, delta_t, robot_actual, departure_W, arrive, true, &trajectories_noise);
-
-
-            /** Traverse all curve points between point P0 and P1 **/
-            // while (!trajectories_ideal.empty())
-            // {
-                
-                /** Extract the next curve point **/
-                // Vec2f p_ideal = trajectories_ideal.front();
-                // Vec2f p_actual = trajectories_actual.front();
-                // trajectories_ideal.pop_front();
-                // trajectories_actual.pop_front();
-
-
-
-                // cout << "homo:" << endl << homo_WR << endl;
-                // cout << ( radian_to_degree(acos(homo_WR(0, 0)))  ) << endl << endl;
-
-                // Vec3f pose_W = homo_WR * Vec2f(0, 0).homogeneous();
-                // cout << "pose_W: (" << pose_W.x() << ", " << pose_W.y() << ")" << endl;
-
-                /** Move robot **/
-                // robot_ideal.move_to(p_ideal);
-                // draw_robot_vector(robot_ideal, lidar_msg);
-                // robot_actual.move_to(p_actual);
-
-
-
-                /** Draw Robot's velocity **/
-                // lidar_msg.points_x.push_back(p_ideal.x());
-                // lidar_msg.points_y.push_back(p_ideal.y());
-                // lidar_msg.points_col.push_back(0xFFFF0000);
-                // lidar_msg.points_x.push_back(p_actual.x());
-                // lidar_msg.points_y.push_back(p_actual.y());
-                // lidar_msg.points_col.push_back(0xFF0000FF);
-
-                // lidar_msg.robotLocX = robot_actual.position_in_Wframe.x();
-                // lidar_msg.robotLocY = robot_actual.position_in_Wframe.y();
-                // lidar_msg.timestamp = time_stamp;
-                // lidar_msg.robotAngle = robot_actual.heading_radian;
-
-                /** Simulate scan **/
-                // vector<Vec2f> point_cloud;                      // Vector: intersection points cloud
-                // simulate_scan(point_cloud, robot_actual, wall_segments, laser_sensor, laser_length_noise, laser_angle_noise);
-                // sort(point_cloud.begin(), point_cloud.end(), compare_xy_Vec2f());
-                // point_cloud.erase( unique(point_cloud.begin(), point_cloud.end()), point_cloud.end());
-                // cout << "point cloud size:" << point_cloud.size() << endl;
-
-                /** Update time delta t after one laser scan **/
-
-                /** Draw laser scan points **/
-                // for (vector<Vec2f>::iterator it = point_cloud.begin(); it != point_cloud.end(); ++it) {
-                //     lidar_msg.points_x.push_back(it->x());
-                //     lidar_msg.points_y.push_back(it->y());
-                //     lidar_msg.points_col.push_back(0xFFA500FF); // Orange
-                // }
-
-                /** Successfull Tests **/
-                // Vec2f F1 = Vec2f(0, 0);
-                // Vec2f F2 = Vec2f(4, 0);
-                // Vec2f F1_frameF2 = Vec2f(-4, -3);
-                // Vec2f F3 = Vec2f(2, 4);
-                // Vec2f F3_frameF2 = Vec2f(0, 2);
-                // Vec2f P9 = Vec2f(5, 1);
-                // Mat3f A12 = get_homogeneous_transform_between_two_frames(F1.x(), F1.y(), 0, F2.x(), F2.y(), 45);
-                // Mat3f A21 = get_homogeneous_transform_between_two_frames(F2.x(), F2.y(), 45, F1.x(), F1.y(), 0);
-                // Mat3f A23 = get_homogeneous_transform_between_two_frames(F2.x(), F2.y(), 45, F3.x(), F3.y(), 90);
-                // Mat3f A13 = get_homogeneous_transform_between_two_frames(F1.x(), F1.y(), 0, F3.x(), F3.y(), 90);
-                // Eigen::Matrix3f A31 = get_homogeneous_transform(F3, F1);
-                // cout << A12 << endl;
-                // cout << A23 << endl;
-                // cout << A13 << endl;
-                // cout << A31 << endl;
-                // Vec3f F1toF2 = A12 * F1.homogeneous();
-                // Vec3f P9inF2 = A21 * P9.homogeneous();
-                // Vec3f F2toF3 = A23 * A12 * F1.homogeneous();
-                // Vec3f F1toF3 = A13 * F1.homogeneous();
-                // Vec3f F1toF3p = A12 * A23 * F1.homogeneous();
-                // Vec3f F3_in_W_frame = A12 * F3_frameF2.homogeneous();
-                // cout << "-----------------------------------" << endl;
-                // cout << F1 << endl << P9 << endl;
-                // cout << F1toF2 << endl;
-                // cout << P9inF2 << endl;
-                // cout << "-----------------------------------" << endl;
-                // cout << F2 << endl;
-                // cout << F2toF3 << endl;
-                // cout << "-----------------------------------" << endl;
-                // cout << F3 << endl;
-                // cout << F1toF3 << endl;
-                // cout << "-----------------------------------" << endl;
-                // cout << F3 << endl;
-                // cout << F1toF3p << endl;
-                // cout << "-----------------------------------" << endl;
-                // cout << F3 << endl;
-                // cout << F3_in_W_frame << endl;
-                // cout << "-----------------------------------" << endl;
-                // F1toF2 = A12 * F1;
-                // Eigen::Vector3f F1toF2 = Eigen::Vector3f(A12 * F1);
-                // Eigen::Vector3f F2toF3 = Eigen::Vector3f(A23 * F2);
-                // Eigen::Vector3f F1toF3 = Eigen::Vector3f(A23 * A12 * F1);
-                // lidar_msg.points_x.push_back(F1.x());
-                // lidar_msg.points_y.push_back(F1.y());
-                // lidar_msg.points_col.push_back(0xFF00FF00);
-                // lidar_msg.points_x.push_back(F2.x());
-                // lidar_msg.points_y.push_back(F2.y());
-                // lidar_msg.points_col.push_back(0xFF00FF00);
-                // lidar_msg.points_x.push_back(F3.x());
-                // lidar_msg.points_y.push_back(F3.y());
-                // lidar_msg.points_col.push_back(0xFF00FF00);
-
-
-                /** Odometry Test **/
-                // Vec2f O1 = Vec2f(0, 0);
-                // Vec2f O2 = Vec2f(2, 0);
-                // Vec2f O3 = Vec2f(4, 2);
-                // Vec2f O4 = Vec2f(4, 4);
-                // float speed_wheel = 0.1;
-                // float del_t = 0.1;
-
-                // float x_pos = 0;
-                // float y_pos = 0;
-                // float dx_ns = wheel_encoder_dx_noise.gaussian();
-                // float dy_ns = wheel_encoder_dy_noise.gaussian();
-                // for (int i = 0; i <= 100000; ++i) {
-                //     float dx_ns = 0;
-                //     float dy_ns = 0;
-                //     if (i % 3 == 0) {
-                        // dx_ns = fabs(wheel_encoder_dx_noise.gaussian());
-                        // dy_ns = wheel_encoder_dy_noise.gaussian();
-                    // }
-                //     wheel_encoder.simulate_odometry( speed_wheel, del_t, 
-                //                     dx_ns, dy_ns );
-                //                     Vec2f pp = Vec2f(robot_ideal.position_in_Wframe.x()+wheel_encoder.dx, robot_ideal.position_in_Wframe.y()+wheel_encoder.dy);
-                //                     robot_ideal.move_to(pp);
-                //     lidar_msg.points_x.push_back( robot_ideal.position_in_Wframe.x() );
-                //     lidar_msg.points_y.push_back( robot_ideal.position_in_Wframe.y() );
-                //     lidar_msg.points_col.push_back(0xFF55A3B0);
-                // cout << "dtheta:" << wheel_encoder.dtheta_degree << endl;
-                // }
-
-
-                ros::spinOnce();
-                loop_rate.sleep();
-            // }
-            // departure_W = Vec3f(arrive_W);
-            // cout << "angle:" << robot_actual.heading_degree << "\tcurrent:(" << robot_actual.position_in_Wframe.x() 
-            //     << "," << robot_actual.position_in_Wframe.y() << ")" << "\tarrive:(" << arrive.x() << "," << arrive.y() << ")" << endl;
-            // cout << "robot's velocity:" << robot_actual.velocity << endl;
-            // cout << "waypoints.front():" << waypoints.front() << endl;
+            ros::spinOnce();
+            loop_rate.sleep();
         }
 
         cout << "arrived at:(" << robot_ideal.position_in_Wframe.x() << ", " << robot_ideal.position_in_Wframe.y() << ")" << endl;
