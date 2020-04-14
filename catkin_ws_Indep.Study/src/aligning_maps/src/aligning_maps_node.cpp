9b3acbab54bb2fb99d10e31ab9251a8e0ee8c41e
wesnjazz <dongwonpark@umass.edu>
2020-04-14 13:57:00 -0400

    - Added more images into map_data folder for testing SIFT and CannyEdge

--- catkin_ws_Indep.Study/src/aligning_maps/src/aligning_maps_node.cpp
+++ catkin_ws_Indep.Study/src/aligning_maps/src/aligning_maps_node.cpp
@@ -14,9 +14,6 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
     ros::Rate loop_rate(20);
 
-    ifstream image_1;
-    ifstream image_2;
-
     cout << "OpenCV version : " << CV_VERSION << endl;
     cout << "Major version : " << CV_MAJOR_VERSION << endl;
     cout << "Minor version : " << CV_MINOR_VERSION << endl;
@@ -24,13 +21,13 @@ int main(int argc, char **argv)
 
     while(ros::ok())
     {
-        int i = 0;
-        // string s_img1 = "img01_nonoise.jpg";
-        // string s_img2 = "img02_nonoise.jpg";
-
-        Mat im1 = imread("/home/dpark/map-fusion/map_data/shaq01.jpg");
+        Mat im1 = imread("/home/dpark/map-fusion/map_data/guitar04.jpg");
+        // Mat im1 = imread("/home/dpark/map-fusion/map_data/mountain01.jpeg");
+        // Mat im1 = imread("/home/dpark/map-fusion/map_data/robot01.jpg");
+        // Mat im1 = imread("/home/dpark/map-fusion/map_data/shaq01.jpg");
         // Mat im1 = imread("/home/dpark/map-fusion/map_data/img01_nonoise.jpg");
         Mat im2 = imread("/home/dpark/map-fusion/map_data/img02_nonoise.jpg");
+
         Mat im1_gray;
         cvtColor(im1, im1_gray, cv::COLOR_BGR2GRAY);
         Mat im2_gray;
@@ -38,23 +35,7 @@ int main(int argc, char **argv)
 
 
         CannyEdge(im1);
-        SIFT(im1);
-
-
-        // CImg<unsigned char> im1(s_img1.c_str()), im2(s_img2.c_str());
-        // CImgDisplay im1_disp(im1, "image 1"), im2_disp(im2, "image 2");
-        // int scale = 3;
-        // im1_disp.resize(im1.width() / scale, im1.height() / scale);
-        // im2_disp.resize(im2.width() / scale, im2.height() / scale);
-
-        // while(!im1_disp.is_closed() && !im2_disp.is_closed())
-        // {
-        //     im1_disp.wait();
-        //     im2_disp.wait();
-        // }
-
-
-
+        // SIFT(im1);
 
 
         cout << "Press any key to continue...";
