e85840c2f2fca768766aa5b79b5c5359148e0ebd
wesnjazz <wesnjazz@gmail.com>
2020-10-04 19:15:21 -0400

    Simple SSD and single line segment comparison gives wrong matches when there is small overlap areas

--- codepractice/opencv_practice/hough01/hough03.cpp
+++ codepractice/opencv_practice/hough01/hough03.cpp
@@ -8,6 +8,12 @@
 using namespace cv;
 using namespace std;
 
+
+double euclidDist(const cv::Vec4i& line)
+{
+    return std::sqrt(static_cast<double>((line[0] - line[2]) * (line[0] - line[2]) + (line[1] - line[3]) * (line[1] - line[3])));
+}
+
 int main(int argc, char **argv)
 {
 
@@ -26,10 +32,10 @@ int main(int argc, char **argv)
     int win_height = 1440 * 0.5;
     namedWindow("disp", WINDOW_NORMAL);
     resizeWindow("disp", win_width, win_height);
-    imshow("disp", src1);
-    waitKey();
-    imshow("disp", src2);
-    waitKey();
+    // imshow("disp", src1);
+    // waitKey();
+    // imshow("disp", src2);
+    // waitKey();
 
     // Copy edges to the images that will display the results in BGR
     cvtColor(src1, dst1, COLOR_GRAY2BGR);
@@ -54,19 +60,56 @@ int main(int argc, char **argv)
         Vec4i l = linesP2[i];
         line( dst2, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 1, LINE_AA);
     }
+
+    double threshold = 50.0;
+    for( size_t i = 0; i < linesP1.size(); i++ )
+    {
+        Mat dstc1 = dst1.clone();
+        Mat dstc2 = dst2.clone();
+        Vec4i l = linesP1[i];
+        Vec4i m;
+        double min_distance = 9999999;
+        int min_idx = -1;
+        for( size_t j = 0; j < linesP2.size(); j++ ) 
+        {
+            m = linesP2[j];
+            double distance = sqrt( pow((l[0]-m[0]),2) + pow((l[1]-m[1]),2)
+                                  + pow((l[2]-m[2]),2) + pow((l[3]-m[3]),2) );
+            if( distance < min_distance)
+            {
+                min_distance = distance;
+                min_idx = j;
+            }
+            cout << distance << "\t";
+        }
+
+        m = linesP2[min_idx];
+        double diff = euclidDist(l) - euclidDist(m);
+        cout << "diff: " << diff << "\t";
+        if( diff > threshold) { continue; }
+        cout << endl 
+            << "Vec4i " << l << " --- "
+        << "min: " << min_distance << " at " << min_idx << " : " << m << endl;
+        line( dstc1, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,255,0), 20, LINE_AA);
+        line( dstc2, Point(m[0], m[1]), Point(m[2], m[3]), Scalar(0,255,255), 20, LINE_AA);
+        cv::namedWindow("src1", WINDOW_NORMAL);
+        cv::namedWindow("src2", WINDOW_NORMAL);
+        cv::namedWindow("Detected Lines (in red) - Probabilistic Line Transform dst1", WINDOW_NORMAL);
+        cv::namedWindow("Detected Lines (in red) - Probabilistic Line Transform dst2", WINDOW_NORMAL);
+        cv::resizeWindow("src1", win_width, win_height);
+        cv::resizeWindow("src2", win_width, win_height);
+        cv::resizeWindow("Detected Lines (in red) - Probabilistic Line Transform dst1", win_width, win_height);
+        cv::resizeWindow("Detected Lines (in red) - Probabilistic Line Transform dst2", win_width, win_height);
+        imshow("src1", src1);
+        imshow("src2", src2);
+        imshow("Detected Lines (in red) - Probabilistic Line Transform dst1", dstc1);
+        imshow("Detected Lines (in red) - Probabilistic Line Transform dst2", dstc2);
+        waitKey();
+    }
+
+
+
+
     // Show results
-    cv::namedWindow("src1", WINDOW_NORMAL);
-    cv::namedWindow("src2", WINDOW_NORMAL);
-    cv::namedWindow("Detected Lines (in red) - Probabilistic Line Transform dst1", WINDOW_NORMAL);
-    cv::namedWindow("Detected Lines (in red) - Probabilistic Line Transform dst2", WINDOW_NORMAL);
-    cv::resizeWindow("src1", win_width, win_height);
-    cv::resizeWindow("src2", win_width, win_height);
-    cv::resizeWindow("Detected Lines (in red) - Probabilistic Line Transform dst1", win_width, win_height);
-    cv::resizeWindow("Detected Lines (in red) - Probabilistic Line Transform dst2", win_width, win_height);
-    imshow("src1", src1);
-    imshow("src2", src2);
-    imshow("Detected Lines (in red) - Probabilistic Line Transform dst1", dst1);
-    imshow("Detected Lines (in red) - Probabilistic Line Transform dst2", dst2);
-    waitKey();
     return 0;
 }
